a9ce61d3056668bffc54dc7075e97fa7e3eb93e5
Aryan Prajapati <aryan.prajapati@bacancy.com>
2025-04-03 11:52:17 +0530

    button color changed

--- Demo_POSLink_Integration/POSLinkHelperApp/POSLinkHelperApp/Program.cs
+++ Demo_POSLink_Integration/POSLinkHelperApp/POSLinkHelperApp/Program.cs
@@ -126,10 +126,23 @@ namespace POSLinkHelperApp
 
 
                 // Generatig Amount request
-                AmountRequest amountReq = new AmountRequest { TransactionAmount = (decimal.Parse(totalAmount) * 100).ToString("000000000") }; 
+                AmountRequest amountReq = new AmountRequest { TransactionAmount = (decimal.Parse(totalAmount) * 100).ToString("000000000") };
+
+
+                //POSLinkAdmin.Form.ShowDialogRequest showDialogReq = new POSLinkAdmin.Form.ShowDialogRequest() { Title = "Payment Mode", Button1 = new SdButton() { Name = "credit" }, Button2 = new SdButton() { Name = "debit" }, Button3 = new SdButton() { Name = "" }, Button4 = new SdButton() { Name = "" } };
+                //POSLinkAdmin.Form.ShowDialogResponse showDialogRsp = new POSLinkAdmin.Form.ShowDialogResponse(); // response
+
+                //try
+                //{
+                //    // execution result
+                //    ExecutionResult exe = terminal.Form.ShowDialog(showDialogReq, out showDialogRsp);
+
+                //}
+                //catch (Exception ex) { Console.WriteLine(ex.Message); }
+
 
                 // Showing Credit / Debit options to user
-                POSLinkAdmin.Form.ShowTextBoxRequest showTextBoxReq = new POSLinkAdmin.Form.ShowTextBoxRequest() { Title = "Payment Mode", Text = formattedText, Button1 = new StbButton() { Name = "CREDIT" }, Button2 = new StbButton() { Name = "DEBIT" }, Timeout="200" ,ContinuousScreen = POSLinkAdmin.Const.ContinuousScreen.Default};
+                POSLinkAdmin.Form.ShowTextBoxRequest showTextBoxReq = new POSLinkAdmin.Form.ShowTextBoxRequest() { Title = "Payment Mode", Text = formattedText, Button1 = new StbButton() { Name = "CREDIT", Color = "323284" }, Button2 = new StbButton() { Name = "DEBIT", Color = "323284" }, Timeout = "200", ContinuousScreen = POSLinkAdmin.Const.ContinuousScreen.Default };
                 POSLinkAdmin.Form.ShowTextBoxResponse showTextBoxRsp = new POSLinkAdmin.Form.ShowTextBoxResponse();
 
 
